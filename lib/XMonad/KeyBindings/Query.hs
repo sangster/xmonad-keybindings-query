5b44e058c146c3d30aacf008053211b3946acc13
Jon Sangster <jon@ertt.ca>
2020-12-29 16:31:44 -0800

    Remove redundant import

--- lib/XMonad/KeyBindings/Query.hs
+++ lib/XMonad/KeyBindings/Query.hs
@@ -9,7 +9,7 @@
 -- Portability  : unportable
 --
 -- This module provides helper function to query XMonad's state before running
--- an 'X' action.
+-- an "X" action.
 --
 -- > import XMonad.KeyBindings.Query
 -- > main =
@@ -23,33 +23,34 @@
 -- >      ]
 --
 -- This module is heavily inspired by Spencer Janssen and Lukas Mai's work in
--- 'XMonad.ManageHook' and 'XMonad.Hooks.ManageHelpers', respectively.
+-- "XMonad.ManageHook" and "XMonad.Hooks.ManageHelpers", respectively.
 --
 -- Note: This module reimplements some functions from
--- 'XMonad.Hooks.ManageHelpers', with more general type-signatures. They
+-- "XMonad.Hooks.ManageHelpers", with more general type-signatures. They
 -- function identically, so the versions exported from this module can be used
 -- in both cases.
 
 
 module XMonad.KeyBindings.Query (
-    -- * Helpers
-    queryState, queryState',
-    queryFocused, queryFocused',
+    Match(..),
 
-    -- * Combiners
+    -- * Query Helpers
+    -- $helpers
+    queryFocused, queryFocused',
+    queryState, queryState',
+
+    -- * Compositions
+    -- $compose
     composeAll, composeOne,
 
     -- * ???
     (/=?), (<==?), (</=?),
     (-->), (=?), (<&&>), (<||>),
     (-?>), (-->>), (-?>>),
-    title, appName, className, stringProperty,
-
-    Match(..)
+    title, appName, className, stringProperty
   ) where
 
 import Data.Bool (bool)
-import Data.Maybe (maybe)
 import XMonad
 import XMonad.StackSet (peek)
 import XMonad.Hooks.ManageHelpers ((/=?), (<==?), (</=?))
@@ -62,10 +63,38 @@ data Match a = Match Bool a
 
 infixr 0 -?>, -->>, -?>>
 
+{- $helpers
+"queryFocused" is the primary function exported by this module. It allows you to
+supply a "Query" to determine what action to execute based on the currently
+focused window. It's possible that no window has focus, so you must supply an
+action to perform in that case. The "queryFocused'" version of this function
+does nothing if no window has focus.
+
+"queryState" and "queryState'" are more generic versions, if you want to
+evaluation a window other than the one that curretly has focus; however, in this
+case you must provide a function to extract your window from the current
+"XState".
+-}
+
+
+-- | Query the focused window of the current windowset. If no window has focus,
+-- the given "fallback" is returned.
+queryFocused :: X a
+             -> Query a
+             -> X a
+queryFocused = queryState $ peek . windowset
+
+
+-- | Like "queryFocused", but does nothing if the query doesn't match the
+-- currently focused window.
+queryFocused' :: Query ()
+              -> X ()
+queryFocused' = queryFocused idHook
+
 
 -- | Query XMonad's current state and use the given function to extract a
--- 'Window' from it. That window is then passed through the 'Query'. If
--- 'Nothing' is returned from the function, then 'fallback' will be used.
+-- "Window" from it. That window is then passed through the "Query". If
+-- "Nothing" is returned from the function, then "fallback" will be used.
 queryState :: (XState -> Maybe Window)
            -> X a
            -> Query a
@@ -74,47 +103,39 @@ queryState f fallback q = f <$> get
                       >>= maybe fallback (runQuery q)
 
 
--- | Like 'queryState', but does nothing if the given function returns
--- 'Nothing'.
+-- | Like "queryState", but does nothing if the given function returns
+-- "Nothing".
 queryState' :: (XState -> Maybe Window)
             -> Query ()
             -> X ()
 queryState' f = queryState f idHook
 
 
--- | Query the focused window of the current windowset. If no window has focus,
--- the given 'fallback' is returned.
-queryFocused :: X a
-             -> Query a
-             -> X a
-queryFocused = queryState $ peek . windowset
+{- $compose
+"composeOne" and "composeAll" are the same as their counterparts exported by
+"XMonad.Hooks.ManageHelpers" and "XMonad.ManageHook". The versions exported by
+this module have more generic signatures, so you can use them both for
+ManageHooks and this.
+-}
 
-
--- | Like 'queryFocused', but does nothing if the query doesn't match the
--- currently focused window.
-queryFocused' :: Query ()
-              -> X ()
-queryFocused' = queryFocused idHook
-
-
--- | Like 'composeAll', but it stops at the first element that returns a 'Just'.
+-- | Like "composeAll", but it stops at the first element that returns a "Just".
 composeOne :: Monoid a
            => [Query (Maybe a)]
            -> Query a
 composeOne = foldr (\q z -> q >>= maybe z return) idHook
 
 
--- | A helper operator for use in 'composeOne'. It takes a condition and an
--- action; if the condition fails, it returns 'Nothing' from the 'Query' so
--- 'composeOne' will go on and try the next rule.
+-- | A helper operator for use in "composeOne". It takes a condition and an
+-- action; if the condition fails, it returns "Nothing" from the "Query" so
+-- "composeOne" will go on and try the next rule.
 (-?>) :: Query Bool
       -> Query a
       -> Query (Maybe a)
 p -?> f = p >>= bool (pure Nothing) (Just <$> f)
 
 
--- | A helper operator for use in 'composeAll'. It takes a condition and a
--- function taking a grouped datum to action. If 'p' is true, it executes the
+-- | A helper operator for use in "composeAll". It takes a condition and a
+-- function taking a grouped datum to action. If "p" is true, it executes the
 -- resulting action.
 (-->>) :: Monoid b
        => Query (Match a)
@@ -123,10 +144,10 @@ p -?> f = p >>= bool (pure Nothing) (Just <$> f)
 p -->> f = p >>= \(Match b m) -> bool mempty (f m) b
 
 
--- | A helper operator for use in 'composeOne'. It takes a condition and a
--- function taking a groupdatum to action. If 'p' is true, it executes the
--- resulting action. If it fails, it returns 'Nothing' from the 'Query' so
--- 'composeOne' will go on and try the next rule.
+-- | A helper operator for use in "composeOne". It takes a condition and a
+-- function taking a groupdatum to action. If "p" is true, it executes the
+-- resulting action. If it fails, it returns "Nothing" from the "Query" so
+-- "composeOne" will go on and try the next rule.
 (-?>>) :: Monoid b
        => Query (Match a)
        -> (a -> Query b)
